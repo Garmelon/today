f6c249393b61736acd8c61a602abe54e153b8878
Joscha <joscha@plugh.de>
2021-11-17 21:55:40 +0100

    Remove parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 use std::path::PathBuf;
-use std::process;
 
 use structopt::StructOpt;
 
-use crate::parse::Parser;
-use crate::source::SourceFiles;
-
 mod commands;
-mod parse;
-mod source;
 
 #[derive(Debug, StructOpt)]
 pub struct Opt {
@@ -18,26 +12,5 @@ pub struct Opt {
 
 fn main() {
     let opt = Opt::from_args();
-
-    let mut files = SourceFiles::new();
-
-    let (file, content) = match files.load(&opt.file) {
-        Ok(result) => result,
-        Err(e) => {
-            eprintln!("Failed to load file: {}", e);
-            process::exit(1);
-        }
-    };
-
-    let mut parser = Parser::new(file, content);
-
-    let commands = match parser.parse(parse::parse_commands) {
-        Ok(result) => result,
-        Err(es) => {
-            files.emit_all(&es);
-            process::exit(1);
-        }
-    };
-
-    println!("{:#?}", commands);
+    println!("{:#?}", opt);
 }
