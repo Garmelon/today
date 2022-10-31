57da1026c2c0bfd7d163460d326c92f59cb94034
Joscha <joscha@plugh.de>
2022-10-31 17:07:10 +0100

    Use cove lints

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+#![deny(unsafe_code)]
+// Rustc lint groups
 #![warn(future_incompatible)]
 #![warn(rust_2018_idioms)]
+// Rustc lints
+#![warn(noop_method_call)]
+#![warn(single_use_lifetimes)]
+#![warn(trivial_numeric_casts)]
+#![warn(unused_crate_dependencies)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_lifetimes)]
+#![warn(unused_qualifications)]
+// Clippy lints
 #![warn(clippy::all)]
 #![warn(clippy::use_self)]
 
